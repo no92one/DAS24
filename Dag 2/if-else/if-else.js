419d09105379bf356926cda166949952025be002
no92one <linus.lindroth.92@gmail.com>
2024-11-05 20:28:04 +0100

    update

---
+++ Dag 2/if-else/if-else.js
@@ -0,0 +1,31 @@
+let age = 10
+
+// if (age > 80) {
+//     console.log("Du får halva priset på din red bull!")
+// } else if (age > 16) {
+//     console.log("Du får köpa en red bull!")
+// } else {
+//     console.log("Du får inte köpa red bull! Kom tillbaka när du är 16 år!")
+// }
+
+
+// if (age > 16 && age < 80) {
+//     console.log("Du får köpa en red bull!")
+// } else if (age > 80) {
+//     console.log("Du får halva priset på din red bull!")
+// } else {
+//     console.log("Du får inte köpa red bull! Kom tillbaka när du är 16 år!")
+// }
+
+// if (age > 16) {
+//     if (age > 80) {
+//         console.log("Du får halva priset på din red bull!")
+//     } else {
+//         console.log("Du får köpa en red bull!")
+//     }
+// } else {
+//     console.log("Du får inte köpa red bull! Kom tillbaka när du är 16 år!")
+// }
+
+
+console.log("Programmet avslutas!")
---
+++ Dag 2/variabler/variabler.js
@@ -0,0 +1,20 @@
+let text = "Hej Linus!"
+let thisIsMyName = "Hej Linus!"
+let this_is_my_name = "Hej Linus!"
+
+
+// console.log("Hello, world!")
+// console.log(typeof text)
+// console.log(typeof 10)
+
+
+const number = 10
+console.log(number)
+let x = 20
+let y = 15
+
+console.log(number * (x + y))
+
+number = x + y
+console.log(number)
+
