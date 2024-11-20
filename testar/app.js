743c484d78991c0f45116afbd2c77ef0635b4a18
unknown <linus.lindroth.92@gmail.com>
2024-11-20 07:48:13 +0100

    update testar-folder

--- testar/app.js
+++ testar/app.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+const button = document.getElementById("submitButton");
+
+button.addEventListener("click", () => {
+
+    const form = document.getElementById("myForm");
+
+    console.dir(form)
+    console.log(form.elements)
+
+    const formData = {};
+    for (let input of form.elements) {
+        if (input.name) {
+            formData[input.name] = input.value;
+        }
+    }
+    console.log("Formulärdata:", formData);
+});
